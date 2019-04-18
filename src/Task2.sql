57eb611afbce23091000c48f09e9a0430423b60c
vitalyvorobej <limeee.rose@gmail.com>
2019-04-18 18:35:22 +0300

    little-bit changed

--- src/Task2.sql
+++ src/Task2.sql
@@ -4,4 +4,5 @@ UPDATE t_sale_str SET price = 1600 where id_sale_str=2;
 UPDATE t_sale_str SET price = 1700 where id_sale_str=3;
 UPDATE t_sale_str SET price = 1800 where id_sale_str=4;
 UPDATE t_sale_str SET price = 1900 where id_sale_str=5; 
+COMMIT;
 SELECT tss.id_sale_str,tss.price,tw.id_ware,tw.price FROM t_ware tw JOIN t_sale_str tss ON tw.id_ware =tss.id_sale_str WHERE tw.price != tss.price;
